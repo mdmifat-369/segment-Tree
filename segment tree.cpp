21ba3dac85642a0a815412ff98041896299bebff
mdmifat-369 <mdmifat369@gmail.com>
2025-11-24 18:43:20 +0600

    Add files via upload

--- segment tree.cpp
+++ segment tree.cpp
@@ -1,37 +1,67 @@
-struct  segtree{
-    int tree[N * 4];
-    void build(int node , int l, int r){
-        if(l==r){
-            tree[node] = 0;
-            return ;
+#include<bits/stdc++.h>
+#define int long long int
+using namespace std;
+
+const int maxN = 1e5 + 9;
+int a[maxN], t[4 * maxN];
+
+
+void build(int node, int b, int e) {
+    if(b == e) {
+        t[node] = a[b];
+        return;
+    }
+    int mid = (b + e) / 2;
+    build(2*node, b, mid);
+    build(2*node+1, mid+1, e);
+    t[node] = t[2*node] + t[2*node+1];
+}
+
+void update(int node, int b, int e, int i, int v) {
+    if(b == e) {
+        t[node] = v;
+        return;
+    }
+    int mid = (b + e) / 2;
+    if(i <= mid)
+        update(2*node, b, mid, i, v);
+    else
+        update(2*node+1, mid+1, e, i, v);
+        
+    t[node] = t[2*node] + t[2*node+1];
+}
+
+
+int query(int node, int b, int e, int i, int j) {
+    if(j < b || i > e) return 0;       
+    if(b >= i && e <= j) return t[node]; 
+    int mid = (b + e) / 2;
+    return query(2*node, b, mid, i, j) + query(2*node+1, mid+1, e, i, j);
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, q;
+    cin >> n >> q;
+
+    for(int i = 1; i <= n; i++) cin >> a[i];
+    build(1, 1, n);
+
+    while(q--) {
+        int type;
+        cin >> type;
+        if(type == 2) {
+            int l, r;
+            cin >> l >> r;
+            cout << query(1, 1, n, l + 1, r) <<endl;
+        } else {
+            int i, v;
+            cin >> i >> v;
+            update(1, 1, n, i + 1, v);
         }
-        int left = node*2, right = (node * 2) + 1;
-        int mid = (l + r) / 2;
-        build(left, l, mid);
-        build(right, mid+1, r);
-        tree[node] = tree[left] + tree[right];
     }
 
-    void update(int node , int l , int r , int i, int val){
-        if(l == r && l == i){
-            tree[node] = val;
-            return;
-        }
-        if(i > r || i < l) return;
-        int left = node * 2, right = (node * 2) + 1;
-        int mid = (l + r) / 2;
-        update(left, l, mid, i, val);
-        update(right, mid+1, r, i, val);
-        tree[node] = tree[left] + tree[right];
-    }
-
-    int query(int node , int l, int r, int i , int j){
-        if(l >= i && r <= j) return tree[node];
-        if(r < i || l > j) return 0;
-        int left = node*2, right = (node*2) + 1;
-        int mid = (l + r) / 2;
-        return query(left, l, mid, i, j) + query(right, mid+1, r, i, j);
-    }
-};
-
-segtree tree;
+    return 0;
+}
